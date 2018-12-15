06869b3740a7c5a2f00297fa72fdf6e036d47e99
Marcus <m4gicks@gmail.com>
2018-12-15 02:11:37 +0000

    Add client/init.lua Version 0.0.2
    - This will change in a future version ( ⛔️ )

---
+++ lua/novus/client/init.lua
@@ -0,0 +1,136 @@
+--imports--
+local cqueues = require"cqueues"
+local util = require"novus.util"
+local api = require"novus.api"
+local shard = require"novus.shard"
+local cache = require"novus.cache"
+local pcall = pcall
+local require = require
+local unpack = table.unpack 
+local tonumber = tonumber
+--start-module--
+local _ENV = {}
+
+--[[
+A client is: a slice of shard states; an api state; and a cache.
+--]]
+clients = _ENV.clients or {}
+
+cqueues.interpose('novus', function(self) 
+    return clients[self]
+end)
+
+cqueues.interpose('associate', function(self, client) 
+    clients[self] = client
+end)
+
+cqueues.interpose('dispatch', function(self, shard, E, ...) 
+    return clients[self].dispatch[E] and clients[self].dispatch[E](clients[self], shard, E, ...)
+end)
+
+local old_wrap 
+old_wrap = cqueues.interpose('wrap', function(self, ...)
+    return old_wrap(self, function(fn, ...) 
+        local s, e = pcall(fn, ...)
+        if not s then 
+            local id = util.rid()
+            util.error("Had error-%s: %s", id, e)
+            util.error("Traceback-%s: %s", id, debug.traceback())
+            util.throw("Throwing error-%s", id)
+        end
+    end, ...)
+end)
+
+function create(options)
+    local client = {id = util.rid()}
+    util.info("Creating Client-%s", client.id)
+    client.options = util.mergewith({}, options)
+    client.api = api.init{token = client.options.token}
+    client.shards = {}
+    client.loop = cqueues.new()
+    client.id_mutex = util.mutex()
+    client.dispatch = util.default(function(self, _, t) 
+        util.info("Got %q", t)
+    end)
+    function client.dispatch.READY(self, s,t)
+        util.info("Client-%s Shard-%s (%s) online.", self.id, s, self.app.id)
+    end
+    client.mutex = util.mutex()
+    client.loop:associate( client )
+    client.cache = cache.new()
+    return client
+end
+
+local function resolve_shards(client, recommended)
+    if not client.options.sharding then return 0 , recommended - 1, recommended 
+    else 
+        local f, l = unpack(util.map(tonumber, client.options.sharding))
+        local d = l - f + 1
+        if not (util.isint(f) and util.isint(l)) then 
+            return util.throw("Client-%s had shard ids which were not integers?")
+        elseif not (f and l) then 
+            return util.throw("Client-%s has incorrect sharding parameters.", client.id)
+        elseif d < 1 or f < 0 or l < f then 
+            return util.throw("Client-%s has an improper shard range: %s,%s", client.id, f, l)
+        end
+        if d ~= recommended then 
+            util.warn("Client-%s is not using the sharding recommended by discord.", client.id)
+        end
+        return f, l, d
+    end
+end
+
+local function runner(client)
+    client.mutex:lock()
+    local token_nonce = util.hash(client.options.token)
+    util.info("Client-%s is starting using TOKEN-%s.", client.id, token_nonce)
+    local success, data, err = api.get_gateway_bot(client.api)
+    local success2, app, err = api.get_current_application_information(client.api)
+    if success and success2 then 
+        client.app = app
+        local limit = data.session_start_limit
+        util.info("TOKEN-%s has used %d/%d sessions.", token_nonce, limit.total - limit.remaining, limit.total)
+        if limit.remaining > 0 then 
+            local total_shards = data.shards
+            client.gateway = data.url
+            local first, last, total = resolve_shards(client, total_shards)
+            util.info("Client-%s is launching %d shards", client.id, total)
+            for id = first, last do 
+                client.shards[id] = shard.init({
+                     token = client.options.token
+                    ,id = id 
+                    ,gateway = data.url 
+                    ,compress = client.options.compress 
+                    ,total_shard_count = total
+                    ,large_threshold = client.options.large_threshold
+                }, client.id_mutex)
+                client.shards[id].dispatch = client.dispatch
+                shard.connect(client.shards[id])
+            end
+        else 
+            util.warn("TOKEN-%s can no longer identify for %s.",token_nonce, util.Date.Interval(limit.reset_after / 1000))
+        end 
+    else 
+        util.warn("Client-%s failed to get a gateway url from discord: %q", client.id, err)  
+    end
+    client.mutex:unlock()
+end
+
+function run(client)
+    client.loop:wrap(runner, client)
+    local use_driver = client.options.driver or "default"
+    local driver_at = "novus.client.drivers.%s" % use_driver
+    local found, driver = pcall(require, driver_at)
+    if found then 
+        util.info("Client-%s is using %q to run it's main loop.", client.id, driver_at)
+        return driver(client)
+    else 
+        return util.fatal("Client-%s could not find a driver at %q!", client.id, driver_at)
+    end
+end
+
+--end-module--
+return _ENV
+
+
+
--- lua/novus/const.lua
+++ lua/novus/const.lua
@@ -1,7 +1,8 @@
+local setmetatable = setmetatable
 --start-module--
 local _ENV = {}
 
-version = "0.0.1"
+version = "0.0.2"
 homepage = "https://github.com/Mehgugs/novus"
 time_unit = "seconds"
 discord_epoch = 1420070400
@@ -9,10 +10,19 @@ gateway_delay = .5
 identify_delay = 5
 
 api = {
-    baseEndpoint = "https://discordapp.com/api",
-    version = 6,
+     base_endpoint = "https://discordapp.com/api"
+    ,avatar_endpoint = "https://cdn.discordapp.com/avatars/%s/%s.%s"
+    ,default_avatar_endpoint = "https://cdn.discordapp.com/embed/avatars/%s.png"
+    ,emoji_endpoint = "https://cdn.discordapp.com/emojis/%s.%s"
+    ,version = 6
 }
-api.endpoint = ("%s/v%s"):format(api.baseEndpoint, api.version)
+api.endpoint = ("%s/v%s"):format(api.base_endpoint, api.version)
+
+default_avatars = 5
+
+lifetimes = setmetatable({}, {__index = function(l) return l.default end})
+
+lifetimes.default = 1.0
 
 --end-module--
 return _ENV
