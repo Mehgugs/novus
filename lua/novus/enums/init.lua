e07e9f636723c4a7e8ff511f1f50ddcfccf7e542
Marcus <m4gicks@gmail.com>
2019-01-09 03:08:46 +0000

    Added enums
    - This module implements a simple enum which is a table whose entries appear as a key and value. ( 👍 )
    - Added auditlogtype enum for auditlog action types. ( 👍 )

---
+++ lua/novus/enums/init.lua
@@ -0,0 +1,42 @@
+--imports--
+local util = require"novus.util"
+local require = require
+local insert = table.insert
+local setmetatable = setmetatable
+local pairs = pairs
+local pcall = pcall
+
+--start-module--
+local _ENV = {}
+
+local function make_enum(tbl)
+    local new = {}
+    for key, value in pairs(tbl) do 
+        new[key], new[value] = value, key
+    end
+    return new 
+end
+
+local function enum_loader(name)
+    local success, M = pcall(require, name)
+    if success then 
+        return make_enum(M)
+    else 
+        return nil, M 
+    end
+end
+
+setmetatable(_ENV, _ENV)
+
+function __index(self, key)
+    local enum, err = enum_loader(("novus.enums.%s"):format(key))
+    if enum then 
+        self[key] = enum 
+        return enum 
+    else
+        util.throw("%s", err or '')
+    end
+end
+
+--end-module--
+return _ENV
