3f9f48ea5fb309fc6d7dcbae6d0681d0f8f95333
Marcus <m4gicks@gmail.com>
2019-01-19 22:52:14 +0000

    Spec cache/view.lua

--- lua/novus/cache/view.lua
+++ lua/novus/cache/view.lua
@@ -1,32 +1,28 @@
 --imports--
-local util = require"novus.util"
-local setmetatable = setmetatable 
+local getmetatable = getmetatable
+local setmetatable = setmetatable
 local next = next
 --start-module--
 local _ENV = {}
 
-function new(t, f, a, m)
-    return setmetatable({}, {
-         __mode = v
-        ,__index = util.bind(__index, t,f,a,m)
-        ,__pairs = __pairs
-    })
+function new(t, f, a)
+    return setmetatable({__t = t, __f = f, __a = a}, _ENV)
 end
 
-function __index(t, f, a, m, view, key)
+function __index(view, key)
     local raw = view.__t[key]
-    if raw and view.__f(key, raw, view.__a) then 
-        view[key] = m and m(key, raw) or raw
-        return raw 
+    if raw ~= nil then
+        local val = view.__f(key, raw, view.__a)
+        return val or nil
     end
 end
 
 function ignores_value(_, value, comp)
-    return value ~= comp 
+    return value ~= comp and value
 end
 
-function ignores_key(key, _, comp)
-    return key ~= comp 
+function ignores_key(key, value, comp)
+    return key ~= comp and value
 end
 
 function remove(view, value)
@@ -35,43 +31,36 @@ end
 
 function remove_key(view, key)
     return new(view, ignores_key, key)
-end 
+end
 
-local ignore_keys = {
-    __t = true, 
-    __f = true,
-    __a = true,
-    __m = true
-}
+local function view_next(invar, state)
+    if getmetatable(invar) == _ENV then
+        return view_next(invar.__t, state)
+    else
+        return next(invar, state)
+    end
+end
 
 local function view_iter(invar, state)
-    local key = next(invar, state)
-    if ignore_keys[key] then 
-        return view_iter(invar, key)
-    else
-        return key, invar[key]
-    end
+    local key = view_next(invar, state)
+    return key, invar[key]
 end
 
 function __pairs(view)
     return view_iter, view
-end 
+end
 
 function flatten(view)
-    for _ in __pairs(view) do end
+    local out = {}
+    for k,v in __pairs(view) do out[k] = v end
+    return out
 end
 
-function add(view, item)
-    if view.__f == ignores_value and view.__a == item then 
-        return view.__t 
-    end
-end
-local ident = function() return true end
-function from(t, m)
-    return new(t, ident, nil, m)
-end
+identity = function(_, x) return x end
 
-view.identity = ident
+function copy(t)
+    return new(t, identity)
+end
 
 --end-module--
 return _ENV
