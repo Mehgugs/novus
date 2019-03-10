ead0a587fa744eaba905ec73ffe285cf437f5f0e
Marcus <m4gicks@gmail.com>
2019-03-10 00:46:37 +0000

    Untabify enums/channeltype.lua

--- lua/novus/enums/channeltype.lua
+++ lua/novus/enums/channeltype.lua
@@ -1,9 +1,9 @@
 return {
-     text     = 0
-	,private  = 1
-	,voice    = 2
-	,group    = 3
-	,category = 4
-	,news     = 5
-	,store    = 6
+   text     = 0
+  ,private  = 1
+  ,voice    = 2
+  ,group    = 3
+  ,category = 4
+  ,news     = 5
+  ,store    = 6
 }
